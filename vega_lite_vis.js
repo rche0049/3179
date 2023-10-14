901a8708ef2636719a9606f15c5569fc3cc2b5df
rche0049 <rche0049@student.monash.edu>
2023-10-14 19:19:31 +1100

    added header, intro, map

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -3,4 +3,10 @@ vegaEmbed("#bar_chart", vg_1).then(function (result) {
     // Access the Vega view instance
    // (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+var vg_2 = "rating_age_heat_map.vg.json";
+vegaEmbed("#heat_map", vg_2).then(function (result) {
+    // Access the Vega view instance
+   // (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
 
