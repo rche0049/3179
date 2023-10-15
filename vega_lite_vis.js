39e1bb80d2411fc06f4d05a61715513cca511a40
rche0049 <rche0049@student.monash.edu>
2023-10-15 17:42:13 +1100

    format data

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -13,19 +13,11 @@ vegaEmbed("#stacked_bar_chart", vg_3,{"actions":false}).then(function (result) {
     // Access the Vega view instance
    // (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+var vg_4 = "male_female_pie_chart.vg.json";
+vegaEmbed("#pie_chart", vg_4,{"actions":false}).then(function (result) {
+    // Access the Vega view instance
+   // (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
 
-// JavaScript code for Vega-Lite
-const countryDropdown = document.getElementById("country-dropdown");
-countryDropdown.addEventListener("change", function () {
-  const selectedCountry = countryDropdown.value;
-  loadGenderData(selectedCountry);
-});
-
-function loadGenderData(selectedCountry) {
-  // Fetch and process data for the selected country
-  // Create a Vega-Lite specification for the pie chart
-  // Embed the pie chart in the "gender-pie-chart" container
-  // Update the chart with the selected country's data
-}
 
 
