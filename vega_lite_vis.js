f4cc815f429d16026a0317858a53bf6eeffb7c9d
rche0049 <rche0049@student.monash.edu>
2023-10-15 12:37:24 +1100

    fix resolutions

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,12 +1,19 @@
 var vg_1 = "total_grandmaster_bar_chart.vg.json";
-vegaEmbed("#bar_chart", vg_1).then(function (result) {
+vegaEmbed("#bar_chart", vg_1,{"actions":false}).then(function (result) {
     // Access the Vega view instance
    // (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 var vg_2 = "rating_age_heat_map.vg.json";
-vegaEmbed("#heat_map", vg_2).then(function (result) {
+vegaEmbed("#heat_map", vg_2,{"actions":false}).then(function (result) {
     // Access the Vega view instance
    // (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+var vg_3 = "titles_stacked_bar_chart.vg.json";
+vegaEmbed("#stacked_bar_chart", vg_3,{"actions":false}).then(function (result) {
+    // Access the Vega view instance
+   // (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
 
 
